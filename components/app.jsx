e7ba19d0624fb4e087fb3348541ee595b733db7e
sujalpatel2209 <sujalpatel022@gmail.com>
2017-12-29 22:33:44 +0530

    ~ add js images ~ add boostrap live links

---
+++ components/app.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className={'container'}>
+                <div className={'row'}>
+                    <div className={'col-lg-4'}>
+                        <img src={''} />
+                    </div>
+                    <div className={'col-lg-4'}>
+
+                    </div>
+                    <div className={'col-lg-4'}>
+
+                    </div>
+                </div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
