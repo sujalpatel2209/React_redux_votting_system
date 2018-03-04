ed6748832df25bc50228a284e7ff544331a46362
sujalpatel2209 <sujalpatel022@gmail.com>
2018-03-04 15:25:13 +0530

    remove state with store initial state, call dispatch for submit the vote(working on)

--- components/app.jsx
+++ components/app.jsx
@@ -20,39 +20,46 @@ constructor(props) {
 
     submitVote (jsNumber, event) {
 
-        switch (jsNumber){
-            case 1:
-                this.setState({
-                    react: this.state.react+1
-                })
-                break;
-            case 2:
-                this.setState({
-                    vue: 1
-                })
-                break;
-            case 3:
-                this.setState({
-                    angular: 1
-                })
-                break;
-            case 4:
-                this.setState({
-                    nodejs: 1
-                })
-                break;
-            case 5:
-                this.setState({
-                    meteor: 1
-                })
-                break;
-            case 6:
-                this.setState({
-                    ember: 1
-                })
-                break;
-        }
+        store.dispatch({
+            type: 'VOTE',
+            payload: {
+                jsNumber,
+                vote: 1
+            }
+        })
 
+        // switch (jsNumber){
+        //     case 1:
+        //         this.setState({
+        //             react: this.state.react+1
+        //         })
+        //         break;
+        //     case 2:
+        //         this.setState({
+        //             vue: 1
+        //         })
+        //         break;
+        //     case 3:
+        //         this.setState({
+        //             angular: 1
+        //         })
+        //         break;
+        //     case 4:
+        //         this.setState({
+        //             nodejs: 1
+        //         })
+        //         break;
+        //     case 5:
+        //         this.setState({
+        //             meteor: 1
+        //         })
+        //         break;
+        //     case 6:
+        //         this.setState({
+        //             ember: 1
+        //         })
+        //         break;
+        // }
     }
 
     render() {
@@ -66,7 +73,7 @@ constructor(props) {
                 <div className={'row, text-center'}>
                     <div className={'col-lg-2'}>
                         <h2>React</h2>
-                        <p>{this.state.react}</p>
+                        <p>{store.getState().react}</p>
                         {
                             (this.state.react == 0) ? <button onClick={this.submitVote.bind(this, 1)} 
                                 className={'btn btn-primary'}>Vote</button>
@@ -76,7 +83,7 @@ constructor(props) {
                     </div>
                     <div className={'col-lg-2'}>
                         <h2>Vue</h2>
-                        <p>{this.state.vue}</p>
+                        <p>{store.getState().vue}</p>
                         {
                             (this.state.vue == 0) ? <button onClick={this.submitVote.bind(this, 2)} className={'btn btn-primary'}>Vote</button>
                             : <button onClick={this.submitVote.bind(this, 2)} className={'btn btn-success'}>Un Vote</button>
@@ -84,7 +91,7 @@ constructor(props) {
                     </div>
                     <div className={'col-lg-2'}>
                         <h2>Angular</h2>
-                        <p>{this.state.angular}</p>
+                        <p>{store.getState().angular}</p>
                         {
                             (this.state.angular == 0) ? <button onClick={this.submitVote.bind(this, 3)} className={'btn btn-primary'}>Vote</button> 
                             : <button onClick={this.submitVote.bind(this, 3)} className={'btn btn-success'}>Un Vote</button> 
@@ -93,7 +100,7 @@ constructor(props) {
                     </div>
                     <div className={'col-lg-2'}>
                         <h2>NodeJs</h2>
-                        <p>{this.state.nodejs}</p>
+                        <p>{store.getState().nodejs}</p>
                         {
                             (this.state.nodejs == 0) ? <button onClick={this.submitVote.bind(this, 4)} className={'btn btn-primary'}>Vote</button>
                             : <button onClick={this.submitVote.bind(this, 4)} className={'btn btn-success'}>Un Vote</button> 
@@ -102,7 +109,7 @@ constructor(props) {
                     </div>
                     <div className={'col-lg-2'}>
                         <h2>Meteor</h2>
-                        <p>{this.state.meteor}</p>
+                        <p>{store.getState().meteor}</p>
                         {
                             (this.state.meteor == 0) ? <button onClick={this.submitVote.bind(this, 5)} className={'btn btn-primary'}>Vote</button> 
                             : <button onClick={this.submitVote.bind(this, 5)} className={'btn btn-success'}>Un Vote</button>
@@ -111,7 +118,7 @@ constructor(props) {
                     </div>
                     <div className={'col-lg-2'}>
                         <h1>Ember</h1>
-                        <p>{this.state.ember}</p>
+                        <p>{store.getState().ember}</p>
                         {
                             (this.state.ember == 0) ? <button onClick={this.submitVote.bind(this, 6)} className={'btn btn-primary'}>Vote</button>
                             : <button onClick={this.submitVote.bind(this, 6)} className={'btn btn-success'}>Un Vote</button>
--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,43 @@
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
+const initialState = {
+    react: 0,
+    vue: 0,
+    angular: 0,
+    nodejs: 0,
+    meteor: 0,
+    ember: 0
+}
+
+const reducer = (state = initialState, action) => {
     switch(action.type){
         case "VOTE":
-            state = state + action.value;
+            switch (action.payload.jsNumber){
+                case 1:
+                    console.log('react')
+                    break;
+                case 2:
+                console.log('vue')
+                   
+                    break;
+                case 3:
+                console.log('angular')
+                   
+                    break;
+                case 4:
+                console.log('nodejs')
+                   
+                    break;
+                case 5:
+                console.log('meteor')
+                    
+                    break;
+                case 6:
+                console.log('ember')
+                  
+                    break;
+            }
+            console.log(action.payload);
         break;
         case "UNVOTE":
 
@@ -12,15 +46,16 @@ const reducer = (state, action) => {
     return state;
 }
 
-const store = createStore(reducer, 1);
+const store = createStore(reducer);
 
+// After Store Updated Execute.
 store.subscribe(() => {
-    console.log('Store Updated : ', store.getState());
+    console.log('Store Updated : ', store.getState().react);
 })
 
-store.dispatch({
-    type: 'VOTE',
-    value: 11
-})
+// store.dispatch({
+//     type: 'VOTE',
+//     value: 11
+// })
 
 export default store;
